447b01ecb1e97c0433348cf3618901898b7e38d8
Benny Johnson <BtpPrograms@gmail.com>
2016-10-18 22:42:50 -0400

    Added missing newline to PrintLengthLines.c
    Completed exercise 1-18

--- Ch1/LineWhitespaceCleanup.c
+++ Ch1/LineWhitespaceCleanup.c
@@ -4,14 +4,18 @@
 
 void copy(char to[], char from[]);
 int get_line(char line[], int lim);
+int remove_trailing_whitespace(char line[], int lim);
 
 int main() {
     int length = 0;
     char line[MAXLINE];
 
     // Iterates until a line with no characters is reached
-    while ((length = get_line(line, MAXLINE)) > 0) {
-       printf("%d Chars: %s\n", length, line); 
+    while ((length = get_line(line, MAXLINE)) > 0) { 
+        length = remove_trailing_whitespace(line, length);
+        if (length > 0) {
+            printf("%d Chars: %s\n", length, line); 
+        }
     }
     return 0;
 }
@@ -33,6 +37,22 @@ int get_line(char line[], int lim) {
     // Adds null terminator and returns completed string
     line[length] = '\0';
     return length;
+}
+
+// This function modifies the original line array directly by replacing trailing whitespace with null terminators
+// Returns the length of the string after whitespace removal or 0 if the line was blank
+int remove_trailing_whitespace(char line[], int length) {
+    int index = length - 1;
+    while (index >= 0) {
+        if (line[index] == ' ' || line[index] == '\n' || line[index] == '\t') {
+            line[index] = '\0';
+            index--;
+        }
+        else {
+            return index + 1;
+        }
+    }
+    return 0;
 }
 
 // This function assumes that the size of to is greater than or equal to the size of from
