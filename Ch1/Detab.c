e0f6d20546a4b7f127acbf7a43318c9965e8bbee
Benny Johnson <bennyaj@umich.edu>
2016-10-23 22:35:16 -0400

    Completed exercise 1-20

---
+++ Ch1/Detab.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+#define TABSTOP 4
+
+int main() {
+    int c;
+    while ((c = getchar()) != EOF) {
+        if (c == '\t') {
+            for (int i = 0; i < TABSTOP; i++) {
+                putchar(' ');
+            } 
+        }
+        else {
+            putchar(c);
+        }
+    }
+    return 0;
+}
